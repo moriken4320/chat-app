1f434222e2783f86f4abb6da18f2e7da0e1e32f3
moriken4320 <moriken.4320@gmail.com>
2020-10-29 14:21:19 +0900

    Active Storageの導入

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,7 @@
 class Message < ApplicationRecord
-  validates :content, presence: true
   belongs_to :user
   belongs_to :room
+  has_one_attached :image
+  
+  validates :content, presence: true
 end
